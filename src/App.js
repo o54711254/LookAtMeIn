d5d4af4b9a0854aa92d4d98b4ef294fb84d8ef5d
gyuri <jinry0034@naver.com>
2024-01-23 17:11:38 +0900

    feat:로그인/회원사입

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import MyPage from "./components/User/MyPage/MyPage";
+import Login from "./components/Sign/Login";
+import Regist from "./components/Sign/Regist";
 
 function App() {
   return (
-    <div>
-      <MyPage />
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="chatbot" element={<ChatBot />} /> */}
+
+        <Route path="mypage" element={<MyPage />} />
+
+        <Route path="login" element={<Login />} />
+
+        <Route path="regist" element={<Regist />} />
+      </Routes>
+    </Router>
   );
 }
 
