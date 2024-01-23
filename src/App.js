fba55d4af6dae2fb0a493d54bee6ca63a42e7d13
오건영 <o54711254@gmail.com>
2024-01-23 13:18:23 +0900

    feat : 마이페이지

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import MyPage from "./components/User/MyPage/MyPage";
-import MyWish from "./components/User/MyPage/MyWish";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Link to={"/mypage"}>마이페이지</Link>
-        <Link to={"/mywish"}>찜</Link>
+
         <Routes>
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/mywish" element={<MyWish />} />
+          <Route path="/mypage/*" element={<MyPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/User/MyPage/MyPage.js
+++ src/components/User/MyPage/MyPage.js
@@ -1,4 +1,17 @@
+import { Routes, Link, Route } from "react-router-dom";
+import MyWish from "./MyWish";
+import MyPostList from "./MyPostList";
+
 function MyPage() {
-  return <div>마이페이지</div>;
+  return (
+    <div>
+      <Link to={"/mypage/mywish"}>찜</Link>
+      <Link to={"/mypage/mypost"}>내가 쓴 게시물</Link>
+      <Routes>
+        <Route path="mywish" element={<MyWish />} />
+        <Route path="mypost" element={<MyPostList />} />
+      </Routes>
+    </div>
+  );
 }
 export default MyPage;
