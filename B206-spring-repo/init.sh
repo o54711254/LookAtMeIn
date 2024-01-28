f7d3344e2be7a3d7554a0ed30662be9de2e8a31e
George Polya <wlskaka4@gmail.com>
2024-01-28 16:44:46 +0900

    fix: jenkins

--- B206-spring-repo/init.sh
+++ B206-spring-repo/init.sh
@@ -4,4 +4,4 @@ sudo docker build --no-cache -t spring-image .
 
 # sudo docker network create -d bridge test-bridge
 
-sudo docker run -p 7000:80 -d --name spring-container --net test-bridge spring-image
+sudo docker run -p 7000:80 -d --name spring-container spring-image
