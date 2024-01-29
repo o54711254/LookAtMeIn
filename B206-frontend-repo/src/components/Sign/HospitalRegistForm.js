625dd4195b28df1f59b23d20b9bb4033ca1a2117
gyuri <jinry0034@naver.com>
2024-01-29 12:31:33 +0900

    fix

--- B206-frontend-repo/src/components/Sign/HospitalRegistForm.js
+++ B206-frontend-repo/src/components/Sign/HospitalRegistForm.js
@@ -5,11 +5,11 @@ import axiosApi from "axios";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
-
 import logo from "../../assets/lab_logo.png";
 import axiosAPi from "../../api/axiosApi";
-import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import DaumPostcode from "react-daum-postcode";
+import { Modal, Button } from "antd";
 
 //회원가입 시 입력하는 정보 유효성 검사
 const validationSchema = yup.object({
@@ -29,10 +29,6 @@ const validationSchema = yup.object({
     .string()
     .email("올바른 이메일 형식을 입력하세요.")
     .required("이메일을 입력하세요."),
-  homepage: yup
-    .string()
-    .url("올바른 홈페이지 주소를 입력하세요.")
-    .required("홈페이지 주소를 입력하세요."),
   phoneNumber: yup
     .string()
     .matches(/^[0-9]+$/, "숫자만 입력하세요.")
@@ -42,141 +38,330 @@ const validationSchema = yup.object({
 function HospitalRegistForm() {
   const navigate = useNavigate();
 
-  // id: "",
-  //     password: "",
-  //     confirmPassword: "",
-  //     hospitalName: "",
-  //     address: "",
-  //     homepage: "",
-  //     businessRegistrationNumber: "",
-  //     businessRegistrationCertificate: "",
-  //     hospitalOwnerName: "",
-  //     email: "",
-  //     phoneNumber: "",
+  //사용 가능한 정보인지 검사
+  const [useable, setUseable] = useState(null);
 
-  //사업자등록증
+  //아이디 중복체크
+  const checkDuplicateId = (e) => {
+    e.preventDefault();
+    axiosAPi.get(`/user/${formik.values.id}`).then((response) => {
+      console.log(response.data);
+      if (response.status === 200) {
+        //alert("사용 불가한 아이디입니다."); //이미 사용중인 아이디
+        setUseable(false);
+      } else if (response.status === 204) {
+        //alert("사용 가능한 아이디입니다.");
+        setUseable(true);
+      } else {
+        alert("사용 불가한 아이디입니다.");
+      }
+    });
+  };
+
+  // 사업자 등록증
   const [businessRegistrationCertificate, setBusinessRegistrationCertificate] =
-    useState([]);
+    useState(null);
 
-  //사업자등록증 업로드될때 작동함
-  const onChange = (e) => {
-    const uploadimg = e.target.files[0];
-    setBusinessRegistrationCertificate(uploadimg);
+  // 파일 업로드 핸들러
+  const handleFileChange = (e) => {
+    setBusinessRegistrationCertificate(e.target.files[0]);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      id: "",
+      password: "",
+      confirmPassword: "",
+      name: "", // 병원명
+      address: "",
+      url: "",
+      email: "",
+      phoneNumber: "",
+      hashtag: "",
+      info: "", // 병원 소개 멘트
+      open: "", // 오픈 시간
+      close: "", // 닫는 시간
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      const formData = new FormData();
+      formData.append("id", values.id);
+      formData.append("password", values.password);
+      formData.append("name", values.name);
+      formData.append("address", values.address);
+      formData.append("url", values.url);
+      formData.append("email", values.email);
+      formData.append("phoneNumber", values.phoneNumber);
+      formData.append("hashtag", values.hashtag);
+      formData.append(
+        "businessRegistrationCertificate",
+        businessRegistrationCertificate
+      ); //여기까지는 필수 입력 사항
+      //여기부터는 선택적으로 입력하는 사항
+      if (values.info) {
+        formData.append("info", values.info);
+      }
+      if (values.open) {
+        formData.append("open", values.open);
+      }
+      if (values.close) {
+        formData.append("close", values.close);
+      }
+
+      try {
+        await axiosApi.post("/HospitalRegistForm", formData, {
+          //서버에게 클라이언트가 보내는 데이터의 유형을 알려주는 것.
+          headers: {
+            "Content-Type": "multipart/form-data", //multipart/form-data는 폼 데이터가 파일이나 이미지와 같은 바이너리 데이터를 포함할 수 있음을 나타냄
+          },
+        });
+        window.alert("회원가입이 완료되었습니다. 승인을 대기해주세요");
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      } catch (e) {
+        console.log(e.response.data.message);
+      }
+    },
+  });
+
+  // 비밀번호 일치 체크
+  const isPasswordMatch =
+    formik.values.password === formik.values.confirmPassword &&
+    formik.values.password !== "";
+
+  //우편주소 api 사용한 부분
+
+  //modalVisible은 모달이 보여질지 말지를 결정하는 불리언 값
+  //setModalVisible 함수는 이 값을 변경하는 데 사용(showModal, closeModal에서 사용함)
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleAddress = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    formik.setFieldValue("address", fullAddress);
+    setModalVisible(false); // 모달 닫기
+  };
+  //주소 입력 버튼을 클릭했을 때 모달을 보여주기
+  const showModal = () => {
+    setModalVisible(true);
+  };
+  //모달을 닫을 때 사용되며, modalVisible을 false로 설정
+  const closeModal = () => {
+    setModalVisible(false);
   };
 
   return (
     <>
       <div className="hospitalRegistForm">
-        <img src={logo} alt="로고사진" />
-        <div className="LABRegist">룩앳미인</div>
-        <form>
-          <h3>아이디</h3>
-          <input
-            type="text"
-            id="id"
-            placeholder="abcdef"
-            // value={formik.values.id}
-            // onChange={formik.handleChange}
-            // error={formik.touched.id && Boolean(formik.errors.id)}
-            // helperText={formik.touched.id ? formik.errors.id : ""}
-          />
+        <div>
+          <img src={logo} alt="로고사진" />
+          <h1>룩앳미인</h1>
+        </div>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="inputText">
+            <h3>아이디</h3>
+            <input
+              type="text"
+              placeholder="abcd1234"
+              name="id"
+              value={formik.values.id}
+              onChange={formik.handleChange}
+              className={formik.touched.id && formik.errors.id ? "error" : ""}
+            />
+            {formik.touched.id && formik.errors.id && (
+              <div className="helperText">{formik.errors.id}</div>
+            )}
 
-          {/* onClick={checkDuplicateId} */}
-          <button>중복확인</button>
-          {/* {useable === null ? (
-            ""
-          ) : useable ? (
-            <span>사용가능한 아이디입니다.</span>
-          ) : (
-            <span>이미 사용중인 아이디 입니다.</span>
-          )} */}
-          <h3>비밀번호</h3>
-          <input
-            type="password"
-            placeholder="영문, 숫자, 특수문자 조합 8~16자를 입력하세요"
-            // value={formik.values.password}
-            // onChange={formik.handleChange}
-            // error={formik.touched.password && Boolean(formik.errors.password)}
-            // helperText={formik.touched.password ? formik.errors.password : ""}
-            autoComplete="off" //자동 완성을 비활성화
-          />
-          <h3>비밀번호 확인</h3>
-          <input
-            type="password"
-            placeholder="비밀번호 확인"
-            autoComplete="off"
-            // value={formik.values.confirmPassword}
-            // onChange={formik.handleChange}
-            // error={
-            //   formik.touched.confirmPassword &&
-            //   Boolean(formik.errors.confirmPassword)
-            // }
-            // helperText={
-            //   formik.touched.confirmPassword
-            //     ? formik.errors.confirmPassword
-            //     : ""
-            // }
-          />
-          <h3>병원명</h3>
-          <input
-            type="text"
-            placeholder="한글 병원명"
-            // value={formik.values.hospitalName}
-            // onChange={formik.handleChange}
-          />
-          <h3>주소</h3>
-          <input
-            type="text"
-            placeholder="주소 입력"
-            // value={formik.values.address}
-            // onChange={formik.handleChange}
-          />
+            <button onClick={checkDuplicateId}>중복확인</button>
+            {useable === null ? (
+              ""
+            ) : useable ? (
+              <span>사용가능한 아이디입니다.</span>
+            ) : (
+              <span>이미 사용중인 아이디 입니다.</span>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>비밀번호</h3>
+            <input
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              placeholder="영문, 숫자, 특수문자 조합 8~16자를 입력하세요"
+              value={formik.values.password}
+              className={
+                formik.touched.password && formik.errors.password ? "error" : ""
+              }
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className="helperText">{formik.errors.password}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>비밀번호 확인</h3>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              placeholder="비밀번호 확인"
+              style={{ color: isPasswordMatch ? "green" : "inherit" }} // 조건부 스타일 적용
+            />
+            {isPasswordMatch && (
+              <div style={{ color: "green" }}>비밀번호가 일치합니다.</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>병원명</h3>
+            <input
+              type="text"
+              placeholder="병원 이름"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.name && formik.errors.name ? "error" : ""
+              }
+            />
+            {formik.touched.name && formik.errors.name && (
+              <div className="helperText">{formik.errors.name}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>주소</h3>
+            <input
+              type="text"
+              name="address"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              readOnly
+            />
+            <button type="button" onClick={showModal}>
+              주소 입력
+            </button>
+            <Modal
+              title="주소 검색"
+              open={modalVisible}
+              onCancel={closeModal}
+              footer={null}
+            >
+              <DaumPostcode onComplete={handleAddress} />
+            </Modal>
+          </div>
 
-          {/* onClick={handleAddressInput} */}
-          <button>주소 입력</button>
-          <h3>홈페이지</h3>
-          <input
-            type="text"
-            placeholder="URL"
-            // value={formik.values.homepage}
-            // onChange={formik.handleChange}
-          />
-          <h3>사업자 등록번호</h3>
-          <input
-            type="text"
-            placeholder="T사업자 등록번호"
-            // value={formik.values.businessRegistrationNumber}
-            // onChange={formik.handleChange}
-          />
-          <h3>사업자 등록증</h3>
-          <input
-            type="text"
-            placeholder="주소 사업자 등록증"
-            // value={formik.values.businessRegistrationCertificate}
-            // onChange={formik.handleChange}
-          />
-          <button>파일 선택</button>
-          <h3>병원장 이름</h3>
-          <input
-            type="text"
-            placeholder="병원장 이름"
-            // value={formik.values.hospitalOwnerName}
-            // onChange={formik.handleChange}
-          />
-          <h3>이메일</h3>
-          <input
-            type="text"
-            placeholder="abcdef@ssafy.com"
-            // value={formik.values.email}
-            // onChange={formik.handleChange}
-          />
-          <h3>전화번호</h3>
-          <input
-            type="text"
-            placeholder="042-000-1234"
-            // value={formik.values.phoneNumber}
-            // onChange={formik.handleChange}
-          />
+          <div className="inputText">
+            <h3>홈페이지</h3>
+            <input
+              type="text"
+              name="url"
+              placeholder="홈페이지 URL"
+              value={formik.values.url}
+              onChange={formik.handleChange}
+              className={formik.touched.url && formik.errors.url ? "error" : ""}
+            />
+            {formik.touched.url && formik.errors.url && (
+              <div className="helperText">{formik.errors.url}</div>
+            )}
+          </div>
+
+          <div className="inputText">
+            <h3>이메일</h3>
+            <input
+              type="text"
+              name="email"
+              placeholder="이메일"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.email && formik.errors.email ? "error" : ""
+              }
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className="helperText">{formik.errors.email}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>병원 Tel</h3>
+            <input
+              type="text"
+              name="phoneNumber"
+              placeholder="전화번호"
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.phoneNumber && formik.errors.phoneNumber
+                  ? "error"
+                  : ""
+              }
+            />
+            {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+              <div className="helperText">{formik.errors.phoneNumber}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>해시태그</h3>
+            <input
+              type="text"
+              name="hashtag"
+              placeholder="해시태그"
+              value={formik.values.hashtag}
+              onChange={formik.handleChange}
+            />
+          </div>
+          {/* 선택적 필드: 병원 소개, 오픈 시간, 닫는 시간 */}
+          <div className="inputText">
+            <h3>병원 소개 (선택 사항)</h3>
+            <input
+              type="text"
+              name="info"
+              placeholder="병원 소개"
+              value={formik.values.info}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className="inputText">
+            <h3>오픈 시간 (선택 사항)</h3>
+            <input
+              type="text"
+              name="open"
+              placeholder="오픈 시간"
+              value={formik.values.open}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className="inputText">
+            <h3>닫는 시간 (선택 사항)</h3>
+            <input
+              type="text"
+              name="close"
+              placeholder="닫는 시간"
+              value={formik.values.close}
+              onChange={formik.handleChange}
+            />
+          </div>
+          {/* 사업자 등록증 파일 업로드 */}
+          <div className="inputText">
+            <h3>사업자 등록증</h3>
+            <input
+              type="file"
+              name="businessRegistrationCertificate"
+              onChange={handleFileChange}
+            />
+          </div>
+
+          {/* 회원가입 버튼 */}
           <div className="RegistButton">
             <button type="submit">회원가입</button>
           </div>
--- B206-frontend-repo/src/components/Sign/UserRegistForm.js
+++ B206-frontend-repo/src/components/Sign/UserRegistForm.js
@@ -6,6 +6,8 @@ import * as yup from "yup";
 import axiosAPi from "../../api/axiosApi";
 import logo from "../../assets/lab_logo.png";
 import { useNavigate } from "react-router-dom";
+import DaumPostcode from "react-daum-postcode";
+import { Modal, Button } from "antd";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //회원가입 시 입력하는 정보 유효성 검사
@@ -30,7 +32,7 @@ const validationSchema = yup.object({
     .string()
     .matches(/^[0-9]+$/, "숫자만 입력하세요.")
     .required("전화번호를 입력하세요."),
-  birthdate: yup
+  birth: yup
     .string()
     .matches(
       /^(19|20)\d\d\.(0[1-9]|1[0-2])\.(0[1-9]|[12][0-9]|3[01])$/,
@@ -44,6 +46,7 @@ function UserRegistForm() {
   const navigate = useNavigate();
   //사용 가능한 정보인지 검사
   const [useable, setUseable] = useState(null);
+
   //아이디 중복체크
   const checkDuplicateId = (e) => {
     e.preventDefault();
@@ -110,6 +113,43 @@ function UserRegistForm() {
     },
   });
 
+  // 비밀번호 일치 체크
+  const isPasswordMatch =
+    formik.values.password === formik.values.confirmPassword &&
+    formik.values.password !== "";
+
+  //우편주소 api 사용한 부분
+
+  //modalVisible은 모달이 보여질지 말지를 결정하는 불리언 값
+  //setModalVisible 함수는 이 값을 변경하는 데 사용(showModal, closeModal에서 사용함)
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleAddress = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    formik.setFieldValue("address", fullAddress);
+    setModalVisible(false); // 모달 닫기
+  };
+  //주소 입력 버튼을 클릭했을 때 모달을 보여주기
+  const showModal = () => {
+    setModalVisible(true);
+  };
+  //모달을 닫을 때 사용되며, modalVisible을 false로 설정
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <>
       <div className="UserRegistForm">
@@ -118,35 +158,177 @@ function UserRegistForm() {
           <h1>룩앳미인</h1>
         </div>
         <form onSubmit={formik.handleSubmit}>
-          <h3>아이디</h3>
-          <input
-            type="text"
-            placeholder="abcd1234"
-            name="id"
-            value={formik.values.id}
-          />
-          <button>중복확인</button>
-          <h3>비밀번호</h3>
-          <input
-            type="password"
-            placeholder="영문, 숫자, 특수문자 조합 8~16자를 입력하세요"
-          />
-          <h3>비밀번호 확인</h3>
-          <input type="password" placeholder="비밀번호 확인" />
-          <h3>이름</h3>
-          <input type="text" placeholder="한글 이름" />
-          <h3>닉네임</h3>
-          <input type="text" placeholder="닉네임" />
-          <h3>생년월일</h3>
-          <input type="text" placeholder="1999.01.01" />
-          <h3>전화번호</h3>
-          <input type="text" placeholder="010-0000-0000" />
-          <h3>주소</h3>
-          <input type="text" placeholder="주소 입력" />
-          <button>주소 입력</button>
-          <h3>이메일</h3>
-          <input type="text" placeholder="abcdef@ssafy.com" />
+          <div className="inputText">
+            <h3>아이디</h3>
+            <input
+              type="text"
+              placeholder="abcd1234"
+              name="id"
+              value={formik.values.id}
+              onChange={formik.handleChange}
+              className={formik.touched.id && formik.errors.id ? "error" : ""}
+            />
+            {formik.touched.id && formik.errors.id && (
+              <div className="helperText">{formik.errors.id}</div>
+            )}
 
+            <button onClick={checkDuplicateId}>중복확인</button>
+            {useable === null ? (
+              ""
+            ) : useable ? (
+              <span>사용가능한 아이디입니다.</span>
+            ) : (
+              <span>이미 사용중인 아이디 입니다.</span>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>비밀번호</h3>
+            <input
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              placeholder="영문, 숫자, 특수문자 조합 8~16자를 입력하세요"
+              value={formik.values.password}
+              className={
+                formik.touched.password && formik.errors.password ? "error" : ""
+              }
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className="helperText">{formik.errors.password}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>비밀번호 확인</h3>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              placeholder="비밀번호 확인"
+              style={{ color: isPasswordMatch ? "green" : "inherit" }} // 조건부 스타일 적용
+            />
+            {isPasswordMatch && (
+              <div style={{ color: "green" }}>비밀번호가 일치합니다.</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>이름</h3>
+            <input
+              type="text"
+              placeholder="한글 이름"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.name && formik.errors.name ? "error" : ""
+              }
+            />
+            {formik.touched.name && formik.errors.name && (
+              <div className="helperText">{formik.errors.name}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>성별</h3>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="남"
+                  checked={formik.values.gender === "남"}
+                  onChange={formik.handleChange}
+                />
+                남
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="여"
+                  checked={formik.values.gender === "여"}
+                  onChange={formik.handleChange}
+                />
+                여
+              </label>
+            </div>
+            {formik.touched.gender && formik.errors.gender && (
+              <div className="helperText">{formik.errors.gender}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>생년월일</h3>
+            <input
+              type="text"
+              placeholder="YYYY.MM.DD"
+              name="birth"
+              value={formik.values.birth}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.birth && formik.errors.birth ? "error" : ""
+              }
+            />
+            {formik.touched.birth && formik.errors.birth && (
+              <div className="helperText">{formik.errors.birth}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>전화번호</h3>
+            <input
+              type="text"
+              placeholder="01000000000"
+              name="phoneNumber"
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.phoneNumber && formik.errors.phoneNumber
+                  ? "error"
+                  : ""
+              }
+            />
+            {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+              <div className="helperText">{formik.errors.phoneNumber}</div>
+            )}
+          </div>
+          <div className="inputText">
+            <h3>주소</h3>
+            <input
+              type="text"
+              name="address"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              readOnly
+            />
+            <button type="button" onClick={showModal}>
+              주소 입력
+            </button>
+
+            <Modal
+              title="주소 검색"
+              open={modalVisible}
+              onCancel={closeModal}
+              footer={null}
+            >
+              <DaumPostcode onComplete={handleAddress} />
+            </Modal>
+          </div>
+          <div className="inputText">
+            <h3>이메일</h3>
+            <input
+              type="text"
+              placeholder="abcdef@ssafy.com"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className={
+                formik.touched.email && formik.errors.email ? "error" : ""
+              }
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className="helperText">{formik.errors.email}</div>
+            )}
+          </div>
           <div className="RegistButton">
             <button type="submit">회원가입</button>
           </div>
