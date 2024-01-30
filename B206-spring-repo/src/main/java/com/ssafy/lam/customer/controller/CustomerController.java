978e099d4428485f1d31b48c3015385568d4ed5d
George Polya <wlskaka4@gmail.com>
2024-01-31 00:13:01 +0900

    feat: tokenInfo

--- B206-spring-repo/src/main/java/com/ssafy/lam/customer/controller/CustomerController.java
+++ B206-spring-repo/src/main/java/com/ssafy/lam/customer/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.ssafy.lam.customer.controller;
 
 
+import com.ssafy.lam.customer.dto.CustomerTokenInfo;
 import com.ssafy.lam.customer.model.service.CustomerService;
 import com.ssafy.lam.entity.Customer;
+import com.ssafy.lam.entity.TokenInfo;
 import com.ssafy.lam.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
@@ -17,10 +19,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController{
+    private Logger log = LoggerFactory.getLogger(CustomerController.class);
 
     private final CustomerService customerService;
 
-    private Logger log = LoggerFactory.getLogger(CustomerController.class);
 
     @Autowired
     public CustomerController(CustomerService customerService) {
@@ -40,5 +42,44 @@ public class CustomerController{
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/login")
+    @Operation(summary = "고객 로그인")
+    public ResponseEntity<CustomerTokenInfo> loginCustomer(@RequestBody Customer customer) {
+        log.info("loginCustomer customer : {}", customer);
+        Customer customer1 = customerService.findByCustomerId(customer.getUserId());
+        if(customer1 == null){
+            return ResponseEntity.notFound().build();
+        }
+        if(!customer1.getPassword().equals(customer.getPassword())){
+            return ResponseEntity.notFound().build();
+        }
 
+//        String user_type = customerService.getUserType(customer1);
+        TokenInfo tokenInfo = customerService.getLoginToken(customer1);
+        log.info("tokenInfo : {}", tokenInfo);
+        CustomerTokenInfo customerTokenInfo = CustomerTokenInfo.builder()
+                .tokenInfo(tokenInfo)
+//                .user_type(user_type)
+                .userId(customer1.getUserId())
+                .build();
+
+        return ResponseEntity.ok().body(customerTokenInfo);
+    }
+
+
+//  private final CustomerService customerService;
+//
+//  @Autowired
+//    public CustomerController(CustomerService customerService) {
+//        this.customerService = customerService;
+//    }
+//
+//    @PostMapping("/regist")
+//    @Operation(summary = "고객 회원가입")
+//    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer) {
+//
+//        log.info("createCustomer customer : {}", customer);
+//        customerService.createCustomer(customer);
+//        return ResponseEntity.ok().build();
+//    }
 }
