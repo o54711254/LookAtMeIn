82cf2ba81c93d67ad9061b6001bd2d34ac798d13
김희수 <wlskaka4@gmail.com>
2024-01-31 16:56:30 +0900

    rebase

--- B206-spring-repo/src/main/java/com/ssafy/lam/customer/model/repository/CustomerRepository.java
+++ B206-spring-repo/src/main/java/com/ssafy/lam/customer/model/repository/CustomerRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< PATCH SET (700382 feat: user_rolesì와 user_type에 customer들어감. CustomerRepositor)
+
 package com.ssafy.lam.customer.model.repository;
 
 import com.ssafy.lam.entity.Customer;
@@ -11,11 +11,4 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
     Optional<Customer> findByUserId(String customerId);
 
-<<<<<<< PATCH SET (d7d4e8 feat: tokenInfo)
-//    String findUserType(Customer customer);
-=======
-=======
-package com.ssafy.lam.customer.model.repository;public class CustomerRepository {
->>>>>>> BASE      (9bdca5 Merge "feat: user_roles에 customer가 들어감" into dev-spring-hees)
->>>>>>> BASE      (dac024 feat: user_rolesì와 user_type에 customer들어감. CustomerRepositor)
 }
