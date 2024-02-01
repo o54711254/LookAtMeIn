a44ec96c71d62e369b72e65666a90981a4a19647
오건영 <o54711254@gmail.com>
2024-02-01 22:08:23 +0900

    feat : 관리자 페이지 병원 승인, 반려 기능 구현

--- B206-frontend-repo/src/components/Admin/MyPage/HospitalList.js
+++ B206-frontend-repo/src/components/Admin/MyPage/HospitalList.js
@@ -18,20 +18,52 @@ function HospitalList() {
         console.log("병원 목록을 가져오는데 실패했습니다.", error);
       });
   }, [listType]); // listType이 변경될 때마다 useEffect 실행
+  const handleApprove = (hos_seq) => {
+    axiosApi
+      .patch(`/admin/hos/approve/${hos_seq}`)
+      .then((res) => {
+        console.log("승인 성공");
+        setListType("all");
+      })
+      .catch((error) => {
+        console.log("승인 실패", error);
+      });
+  };
+  const handleReject = (hos_seq) => {
+    axiosApi
+      .patch(`/admin/hos/approve/${hos_seq}`)
+      .then((res) => {
+        console.log("승인 성공");
+        setListType("all");
+      })
+      .catch((error) => {
+        console.log("승인 실패", error);
+      });
+  };
 
   return (
     <div>
       <button onClick={() => setListType("all")}>전체 병원 목록</button>
       <button onClick={() => setListType("reject")}>미승인 병원 목록</button>
       {postList.length > 0 ? (
-        <div>
-          {postList.map((post, index) => {
+        <ul>
+          {postList.map((post, index) => (
             <li key={index}>
               <div>{post.hospital_name}</div>
-              <div>{post.statusCode}</div>
-            </li>;
-          })}
-        </div>
+              <div>승인 상태: {post.approve ? "승인됨" : "미승인"}</div>
+              {!post.approve && (
+                <div>
+                  <button onClick={() => handleApprove(post.hos_seq)}>
+                    승인
+                  </button>
+                  <button onClick={() => handleReject(post.hos_seq)}>
+                    반려
+                  </button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
       ) : (
         <div>병원 내용이 없습니다.</div>
       )}
