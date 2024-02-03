9ed2fc89837fc6e98b111cc0c1758c02924216bb
오건영 <o54711254@gmail.com>
2024-02-03 20:52:08 +0900

    feat : 리뷰리스트 -> 리뷰디테일 네비게이트 구현

--- B206-frontend-repo/src/components/ReviewBoard/ReviewList.js
+++ B206-frontend-repo/src/components/ReviewBoard/ReviewList.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axiosApi from "../../api/axiosApi";
 import StarRating from "./StarRating/StarResult";
-// axios완료
+import { useNavigate } from "react-router-dom";
+
 function ReviewList() {
   const [reviewBoardList, setReviewBoardList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axiosApi.get(`/api/reviewBoard/list`).then((res) => {
-      console.log(res.data);
       setReviewBoardList(res.data);
     });
   }, []);
 
-  // 더미 데이터
-  // const reviewBoardList = [{
-  //   customer_id: 'user123',
-  //   reviewBoard_title: '테스트 리뷰',
-  //   reviewBoard_cnt: 100,
-  //   reviewBoard_regDate: '2024-01-28',
-  //   reviewBoard_score: 4,
-  // }]
+  const handleClick = (reviewBoard_seq) => {
+    navigate(`/reviewdetail/${reviewBoard_seq}`);
+  };
 
   return (
     <div>
       <h1>리뷰 보드 목록</h1>
       <div>
         {reviewBoardList.map((board) => (
-          <li key={board.customer_name}>
+          <li
+            key={board.reviewBoard_seq}
+            onClick={() => handleClick(board.reviewBoard_seq)}
+          >
             <div>작성자: {board.customer_name}</div>
             <div>제목: {board.reviewBoard_title}</div>
             <div>조회수: {board.reviewBoard_cnt}</div>
