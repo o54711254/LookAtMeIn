1d59ae6e04d958c77a89a23b414ebec417909bbe
오건영 <o54711254@gmail.com>
2024-02-06 00:42:25 +0900

    feat : 병원 마이페이지 구현

--- B206-frontend-repo/src/components/Hospital/MyPage/ConsultingList.js
+++ B206-frontend-repo/src/components/Hospital/MyPage/ConsultingList.js
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
-import axiosApi from "../../../api/axiosApi";
-import { useSelector } from "react-redux";
+import axiosAPi from "../../../api/axiosApi";
 
-function Coordinator() {
+function ConsultingList() {
   const [postList, setPostList] = useState([]);
-  const user = useSelector((state) => state.user);
   useEffect(() => {
-    axiosApi
-      .get(`/api/post/list/${user}`)
+    axiosAPi
+      .get(`/api/post/list`)
       .then((res) => {
         setPostList(res.data);
       })
       .catch((error) => {
-        console.log("코디네이터 정보를 가져오는데 실패했습니다.", error);
+        console.log("데이터를 가져오는데 실패했습니다.", error);
       });
   });
   return (
@@ -21,14 +19,15 @@ function Coordinator() {
         <ul>
           {postList.map((post, index) => {
             <li key={index}>
-              <div>{post}</div>
+              <div>작성자 : {post.id}</div>
+              <div>내용 : {post.contents}</div>
             </li>;
           })}
         </ul>
       ) : (
-        <div>의사정보가 없습니다.</div>
+        <div>병원상담내역이 없습니다.</div>
       )}
     </div>
   );
 }
-export default Coordinator;
+export default ConsultingList;
