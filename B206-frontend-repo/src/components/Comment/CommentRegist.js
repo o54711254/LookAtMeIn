9f75795933f89a307ad98c07b749f5eab79ce86c
gyuri <jinry0034@naver.com>
2024-02-05 10:25:19 +0900

    feat:댓글 작성 기능 추가

--- B206-frontend-repo/src/components/Comment/CommentRegist.js
+++ B206-frontend-repo/src/components/Comment/CommentRegist.js
@@ -3,16 +3,51 @@ import axiosApi from "../../api/axiosApi";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { UseSelector } from "react-redux";
 function CommentRegist({ freeboardSeq }) {
   const [isRegist, setIsRegist] = useState(false);
+  //user 리덕스에서 로그인한 사용자 userSeq 가져옴
+  const userSeq = useSelector((state) => state.user.userSeq);
 
   const [comment, setComment] = useState({
     comment_content: "",
     freeboard_seq: freeboardSeq,
-    user_id: "",
-    user_email: "",
-    regdate: "",
+    user_seq: userSeq,
   });
-  return <></>;
+
+  const startRegist = () => {
+    setIsRegist(true);
+  };
+
+  const handleRegist = async () => {
+    try {
+      const response = await axiosApi.post(`api/comment/regist`, comment);
+      window.location.reload();
+    } catch (error) {
+      console.log("댓글 작성 에러남 : ", error);
+    }
+  };
+  return (
+    <div>
+      {!isRegist ? (
+        <input
+          type="text"
+          value={comment.comment_content}
+          onFocus={startRegist}
+        ></input>
+      ) : (
+        <>
+          <input
+            type="text"
+            value={comment.comment_content}
+            onChange={(e) =>
+              setComment({ ...comment, comment_content: e.target.value })
+            }
+          ></input>
+          <button onClick={handleRegist}>댓글 작성 완료</button>
+        </>
+      )}
+    </div>
+  );
 }
 export default CommentRegist;
