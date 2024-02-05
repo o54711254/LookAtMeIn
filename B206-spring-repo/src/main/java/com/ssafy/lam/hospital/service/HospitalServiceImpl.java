eedf085921b80cac925873239917bab20be5df78
Junikarp <zieunpk0924@naver.com>
2024-02-05 22:29:02 +0900

    fix : serviceImpl 오류 수정

--- B206-spring-repo/src/main/java/com/ssafy/lam/hospital/service/HospitalServiceImpl.java
+++ B206-spring-repo/src/main/java/com/ssafy/lam/hospital/service/HospitalServiceImpl.java
@@ -1,8 +1,5 @@
 package com.ssafy.lam.hospital.service;
 
-import com.ssafy.lam.hospital.domain.Category;
-import com.ssafy.lam.hospital.domain.CategoryRepository;
-import com.ssafy.lam.user.domain.User;
 import com.ssafy.lam.hospital.domain.Hospital;
 import com.ssafy.lam.hospital.domain.HospitalRepository;
 import com.ssafy.lam.hospital.dto.CategoryDto;
@@ -27,12 +24,11 @@ public class HospitalServiceImpl implements HospitalService {
     private final HospitalRepository hospitalRepository;
     private final UserRepository userRepository;
     private final UserService userService;
-    private final CategoryRepository categoryRepository;
 
     private Logger log = LoggerFactory.getLogger(HospitalServiceImpl.class);
 
     @Override
-    public Hospital createHospital(HospitalDto hospitalDto, List<CategoryDto> categoryDto) {
+    public com.ssafy.lam.hospital.domain.Hospital createHospital(HospitalDto hospitalDto, List<CategoryDto> categoryDto) {
         log.info("createHospital : {}", hospitalDto);
         List<String> roles = new ArrayList<>();
         roles.add("HOSPITAL");
@@ -45,7 +41,8 @@ public class HospitalServiceImpl implements HospitalService {
                 .build();
 
         userService.createUser(user);
-        Hospital hospital = Hospital.builder()
+
+        com.ssafy.lam.hospital.domain.Hospital hospital = com.ssafy.lam.hospital.domain.Hospital.builder()
                 .user(user)
                 .tel(hospitalDto.getHospitalInfo_phoneNumber())
                 .address(hospitalDto.getHospitalInfo_address())
@@ -55,26 +52,15 @@ public class HospitalServiceImpl implements HospitalService {
                 .closeTime(hospitalDto.getHospitalInfo_close())
                 .url(hospitalDto.getHospitalInfo_url())
                 .build();
-        hospital = hospitalRepository.save(hospital);
-        for(CategoryDto category : categoryDto){
-            log.info("category : {}", category);
-            Category categoryEntity = Category.builder()
-                    .part(category.getPart())
-                    .hospital(hospital)
-                    .build();
 
-            categoryRepository.save(categoryEntity);
-
-        }
-
-        return hospital;
+        return hospitalRepository.save(hospital);
     }
 
     @Override
     public HospitalDto getHospital(long userId) {
-        Optional<Hospital> hospitalOptional = hospitalRepository.findByUserUserSeq(userId);
+        Optional<com.ssafy.lam.hospital.domain.Hospital> hospitalOptional = hospitalRepository.findById(userId);
         if (hospitalOptional.isPresent()) {
-            Hospital hospital = hospitalOptional.get();
+            com.ssafy.lam.hospital.domain.Hospital hospital = hospitalOptional.get();
 
             HospitalDto dto = HospitalDto.builder()
                     .hospitalInfo_id(hospital.getUser().getUserId())
@@ -109,6 +95,30 @@ public class HospitalServiceImpl implements HospitalService {
 
         userRepository.save(user);
         return hospitalRepository.save(hospital);
+    }
+
+    ////////////
+
+    @Override
+    public HospitalDetailDto getHospitalInfo(long hospitalSeq) { // 고객이 병원 페이지 조회
+        Optional<Hospital> hospitalOptional = hospitalRepository.findById(hospitalSeq);
+        if (hospitalOptional.isPresent()) {
+            Hospital hospital = hospitalOptional.get();
+            HospitalDetailDto hospitalDetailDto = HospitalDetailDto.builder()
+                    .hospitalInfo_seq(hospitalSeq)
+                    .hospitalInfo_name(hospital.getUser().getName())
+                    .hospitalInfo_phoneNumber(hospital.getTel())
+                    .hospitalInfo_introduce(hospital.getIntro())
+                    .hospitalInfo_address(hospital.getAddress())
+                    .hospitalInfo_open(hospital.getOpenTime())
+                    .hospitalInfo_close(hospital.getCloseTime())
+                    .hospitalInfo_url(hospital.getUrl())
+                    .userSeq(hospital.getUser().getUserSeq())
+                    .build();
+            return hospitalDetailDto;
+        } else {
+            return null;
+        }
     }
 
 }
