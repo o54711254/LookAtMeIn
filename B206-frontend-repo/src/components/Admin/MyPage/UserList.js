02f4630c6dccf0d45ef0bb57baa8eea768fa82c7
오건영 <o54711254@gmail.com>
2024-02-09 20:32:29 +0900

    style : 관리자페이지 유저정보 스타일

--- B206-frontend-repo/src/components/Admin/MyPage/UserList.js
+++ B206-frontend-repo/src/components/Admin/MyPage/UserList.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axiosApi from "../../../api/axiosApi";
+import styles from "./UserList.module.css";
 
 function UserList() {
   const [userList, setUserList] = useState([]);
@@ -15,15 +16,15 @@ function UserList() {
       });
   }, []);
   return (
-    <div>
-      <div>유저목록</div>
-      <div>
+    <div className={styles.userListContainer}>
+      <div className={styles.head}>유저목록</div>
+      <div className={styles.userItemContainer}>
         {userList.map((user) => (
-          <div>
-            <div>userNo : {user.userSeq}</div>
-            <div>ID : {user.userId}</div>
-            <div>이름 : {user.name}</div>
-            <div>회원유형 : {user.roles}</div>
+          <div className={styles.userItem}>
+            <div className={styles.items}>userNo : {user.userSeq}</div>
+            <div className={styles.items}>ID : {user.userId}</div>
+            <div className={styles.items}>이름 : {user.name}</div>
+            <div className={styles.items}>회원유형 : {user.roles}</div>
           </div>
         ))}
       </div>
