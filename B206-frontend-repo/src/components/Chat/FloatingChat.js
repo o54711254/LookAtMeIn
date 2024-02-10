43782049c02ce9e36f36f7f64a6e775bf2531117
gyuri <jinry0034@naver.com>
2024-02-11 04:04:15 +0900

    style:2/11

--- B206-frontend-repo/src/components/Chat/FloatingChat.js
+++ B206-frontend-repo/src/components/Chat/FloatingChat.js
@@ -5,6 +5,13 @@ import axiosApi from "../../api/axiosApi";
 import { useSelector } from "react-redux";
 import { Link, Routes, Route, BrowserRouter } from "react-router-dom"; // Link 컴포넌트 추가
 import ChatApp from "./ChatApp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./FloatingChat.module.css";
+
+import {
+  faCommentDots,
+  faCommentSlash,
+} from "@fortawesome/free-solid-svg-icons";
 function FloatingChat() {
   const [open, setOpen] = useState(false);
   const [chatRooms, setChatRooms] = useState([]); // 채팅방 목록 상태
@@ -32,7 +39,13 @@ function FloatingChat() {
         <Paper
           style={{ position: "fixed", bottom: 10, right: 10, zIndex: 1000 }}
         >
-          <Button onClick={handleToggleChat}>{open ? "숨기기" : "채팅"}</Button>
+          <Button onClick={handleToggleChat}>
+            {open ? (
+              <FontAwesomeIcon icon={faCommentSlash} className={styles.icon} />
+            ) : (
+              <FontAwesomeIcon icon={faCommentDots} className={styles.icon} />
+            )}
+          </Button>
           {open && (
             <Box sx={{ p: 10, maxWidth: 300 }}>
               <div>
