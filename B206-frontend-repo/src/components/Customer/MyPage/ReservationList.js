2aeb1231f17ed8176a17028d221d34b514a661ac
오건영 <o54711254@gmail.com>
2024-02-14 15:58:16 +0900

    style : 상담예약 css

--- B206-frontend-repo/src/components/Customer/MyPage/ReservationList.js
+++ B206-frontend-repo/src/components/Customer/MyPage/ReservationList.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux"; // useSelector를 불러옵니다.
 import Questionnaire from "../../Modal/Questionnaire";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import ReservationDetail from "./ReservationDetail";
+import styles from "./ReservationList.module.css";
 // Axios 연결 확인 완료
 
 function ReservationList() {
@@ -29,35 +30,33 @@ function ReservationList() {
   };
 
   return (
-    <div>
-      <h2>Reservation List</h2>
-      <div>
-        {reservations.map((reservation) => (
-          <div onClick={() => goDetailPage(reservation.reserveSeq)}>
-            {/* <p>Customer Name: {reservation.customerName}</p> */}
-            <p>Hospital Name: {reservation.hospitalName}</p>
-
-            <p>
-              상담 예약 날짜: {reservation.year}-
-              {String(reservation.month).padStart(2, "0")}-
-              {String(reservation.day).padStart(2, "0")}
-            </p>
-            <p>상담 예약 요일: {reservation.dayofweek}</p>
-            <p>
-              상담 예약 시간:{" "}
-              {reservation.time < 12
-                ? `${reservation.time} AM`
-                : `${
-                    reservation.time === 12
-                      ? reservation.time
-                      : reservation.time - 12
-                  } PM`}
-            </p>
-            <Questionnaire/>
-            <button>이동</button>
+    <div className={styles.reserveContainer}>
+      {reservations.map((reservation) => (
+        <div
+          onClick={() => goDetailPage(reservation.reserveSeq)}
+          className={styles.reserveItem}
+        >
+          {/* <p>Customer Name: {reservation.customerName}</p> */}
+          <div className={styles.hospitalName}>{reservation.hospitalName}</div>
+          <div>
+            상담 예약 날짜: {reservation.year}-
+            {String(reservation.month).padStart(2, "0")}-
+            {String(reservation.day).padStart(2, "0")}
           </div>
-        ))}
-      </div>
+          <div>{reservation.dayofweek}</div>
+          <div>
+            {" "}
+            {reservation.time < 12
+              ? `${reservation.time} AM`
+              : `${
+                  reservation.time === 12
+                    ? reservation.time
+                    : reservation.time - 12
+                } PM`}
+          </div>
+          <Questionnaire className={styles.button} />
+        </div>
+      ))}
 
       <Routes>
         <Route path="detail/:reserveSeq" element={<ReservationDetail />} />
