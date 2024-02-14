bfc5d35649b783ab7d5da13939379c6e94527d53
오건영 <o54711254@gmail.com>
2024-02-14 19:47:13 +0900

    test : 상담예약 테스트

--- B206-frontend-repo/src/components/Customer/MyPage/ReservationDetail.js
+++ B206-frontend-repo/src/components/Customer/MyPage/ReservationDetail.js
@@ -1,38 +1,58 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosApi from "../../../api/axiosApi";
+import { useSelector } from "react-redux";
+import styles from "./ReservationDetail.module.css";
 
 function ReservationDetail() {
   const [reserveDetail, setReserveDetail] = useState({});
   const { reserveSeq } = useParams();
   const [questionnaire, setQuestionnaire] = useState({});
-
+  const user = useSelector((state) => state.user);
+  const [meetingData, setMeetingData] = useState({
+    userSeq: "",
+    hospitalSeq: "",
+    role: user.role,
+    reserveSeq: "",
+  });
   useEffect(() => {
     axiosApi
       .get(`/api/reserve/detail/${reserveSeq}`)
       .then((res) => {
         console.log("예약상세", res.data);
         setReserveDetail(res.data);
+        setMeetingData((prevData) => ({
+          ...prevData,
+          userSeq: res.data.customerUserSeq,
+          hospitalSeq: res.data.hospitalUserSeq,
+          reserveSeq: res.data.reserveSeq,
+        }));
+        console.log("미팅데이터", meetingData);
         setQuestionnaire(res.data.questionnaireResponseDto);
-        console.log("문진표내용", questionnaire);
       })
       .catch((error) => {
         console.log("안됨", error);
       });
   }, []);
+
   return (
-    <div>
-      <div>{reserveDetail.customerName}</div>
-      <div>{reserveDetail.hopitalName}</div>
-      <div>{reserveDetail.year}</div>
-      <div>{reserveDetail.month}</div>
-      <div>{reserveDetail.day}</div>
-      <div>{reserveDetail.dayofweek}</div>
-      <div>{reserveDetail.time - 12}</div>
-      <div>문진표</div>
-      <div>{questionnaire.blood}</div>
-      <div>{questionnaire.remark}</div>
-      <img src={questionnaire.base64} />
+    <div className={styles.detailContainer}>
+      <div className={styles.top}>
+        <div className={styles.title}>{reserveDetail.hospitalName}</div>
+        <div className={styles.date}>
+          <div>예약시간 : </div>
+          <div>{reserveDetail.year}년</div>
+          <div>{reserveDetail.month}월</div>
+          <div>{reserveDetail.day}일</div>
+          <div>{reserveDetail.dayofweek}</div>
+          <div>{reserveDetail.time - 12}시</div>
+        </div>
+      </div>
+      <div className={styles.questionnaire}>
+        <img src={questionnaire.base64} className={styles.questionImg} />
+        <div>{questionnaire.blood}</div>
+        <div>{questionnaire.remark}</div>
+      </div>
     </div>
   );
 }
