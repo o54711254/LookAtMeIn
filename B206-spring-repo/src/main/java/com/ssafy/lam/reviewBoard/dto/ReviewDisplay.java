46467bad3f0fb5f652e777715a9bcabcf70add02
박하윤 <maha97523@gmail.com>
2024-02-14 22:53:00 +0900

    fix: 후기 게시판 상세 조회 시 고객 번호 출력

--- B206-spring-repo/src/main/java/com/ssafy/lam/reviewBoard/dto/ReviewDisplay.java
+++ B206-spring-repo/src/main/java/com/ssafy/lam/reviewBoard/dto/ReviewDisplay.java
@@ -13,6 +13,7 @@ public class ReviewDisplay {
     private String reviewBoard_content; // 내용
     private double reviewBoard_score; // 별점
     private String customer_name; // 작성자
+    private Long user_seq; // 작성자 번호
     private String reviewBoard_doctor; // 시술 의사
     private String reviewBoard_region; // 지역
     private String reviewBoard_surgery; // 시술 부위
@@ -27,12 +28,13 @@ public class ReviewDisplay {
     private String customerProfileType;
 
     @Builder
-    public ReviewDisplay(Long reviewBoard_seq, String reviewBoard_title, String reviewBoard_content, double reviewBoard_score, String customer_name, String reviewBoard_doctor, String reviewBoard_region, String reviewBoard_surgery, String reviewBoard_hospital, int reviewBoard_expected_price, int reviewBoard_surgery_price, int reviewBoard_cnt, String base64, String imageType) {
+    public ReviewDisplay(Long reviewBoard_seq, String reviewBoard_title, String reviewBoard_content, double reviewBoard_score, String customer_name, Long user_seq, String reviewBoard_doctor, String reviewBoard_region, String reviewBoard_surgery, String reviewBoard_hospital, int reviewBoard_expected_price, int reviewBoard_surgery_price, int reviewBoard_cnt, String base64, String imageType) {
         this.reviewBoard_seq = reviewBoard_seq;
         this.reviewBoard_title = reviewBoard_title;
         this.reviewBoard_content = reviewBoard_content;
         this.reviewBoard_score = reviewBoard_score;
         this.customer_name = customer_name;
+        this.user_seq = user_seq;
         this.reviewBoard_doctor = reviewBoard_doctor;
         this.reviewBoard_region = reviewBoard_region;
         this.reviewBoard_surgery = reviewBoard_surgery;
