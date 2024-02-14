376c96c79482730460e47e188acf86b4ba141ad0
오건영 <o54711254@gmail.com>
2024-02-14 17:42:05 +0900

    feat : 문진표 내용 확인

--- B206-frontend-repo/src/components/Customer/MyPage/ReservationDetail.js
+++ B206-frontend-repo/src/components/Customer/MyPage/ReservationDetail.js
@@ -3,8 +3,9 @@ import { useParams } from "react-router-dom";
 import axiosApi from "../../../api/axiosApi";
 
 function ReservationDetail() {
-  const [reserveDetail, setReserveDetail] = useState(null);
+  const [reserveDetail, setReserveDetail] = useState({});
   const { reserveSeq } = useParams();
+  const [questionnaire, setQuestionnaire] = useState({});
 
   useEffect(() => {
     axiosApi
@@ -12,11 +13,27 @@ function ReservationDetail() {
       .then((res) => {
         console.log("예약상세", res.data);
         setReserveDetail(res.data);
+        setQuestionnaire(res.data.questionnaireResponseDto);
+        console.log("문진표내용", questionnaire);
       })
       .catch((error) => {
         console.log("안됨", error);
       });
   }, []);
-  return <div>d</div>;
+  return (
+    <div>
+      <div>{reserveDetail.customerName}</div>
+      <div>{reserveDetail.hopitalName}</div>
+      <div>{reserveDetail.year}</div>
+      <div>{reserveDetail.month}</div>
+      <div>{reserveDetail.day}</div>
+      <div>{reserveDetail.dayofweek}</div>
+      <div>{reserveDetail.time - 12}</div>
+      <div>문진표</div>
+      <div>{questionnaire.blood}</div>
+      <div>{questionnaire.remark}</div>
+      <img src={questionnaire.base64} />
+    </div>
+  );
 }
 export default ReservationDetail;
