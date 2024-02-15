bf3425ed31b2559424916c68cfdd011373de56b2
gyuri <jinry0034@naver.com>
2024-02-15 21:12:54 +0900

    fix:자유게시판 버튼 보이는 유저 구분

--- B206-frontend-repo/src/components/FreeBoard/FreeBoardDetail.js
+++ B206-frontend-repo/src/components/FreeBoard/FreeBoardDetail.js
@@ -7,6 +7,7 @@ import FreeBoardUpdate from "./FreeBoardUpdate";
 import Comment from "../Comment/Comment";
 import styles from "./FreeBoardDetail.module.css";
 import profile from "../../assets/profile2.png";
+import { useSelector } from "react-redux";
 
 function FreeBoardDetail() {
   const [post, setPost] = useState(null);
@@ -14,6 +15,11 @@ function FreeBoardDetail() {
   const [profileImg, setProfileImg] = useState(null);
   const { freeboardSeq } = useParams();
   const navigate = useNavigate();
+  const userRole = useSelector((state) => state.user.role);
+  const userSeq = useSelector((state) => state.user.userSeq);
+
+  const canEditOrDelete =
+    userRole === "ADMIN" || userSeq === reviewDetail.user_seq;
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -55,11 +61,16 @@ function FreeBoardDetail() {
   return (
     <div className={styles.FreeBoardContainer}>
       <div className={styles.Head}>
-        <img src={profileImg} className={styles.profileImg} />
-        <div className={styles.headBox}>
-          <div className={styles.headInfo1}>
+        <div className={styles.writer}>
+          <img src={profileImg} alt="프로필" className={styles.profileImg} />
+          <div className={styles.idEmail}>
             <div className={styles.userId}>{post.userId}</div>
-            <div className={styles.buttons}>
+            <div className={styles.email}>{post.userEmail}</div>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          {canEditOrDelete && (
+            <div className={styles.bb}>
               <FreeBoardUpdate
                 freeboardContent={post.freeboardContent}
                 freeboardTitle={post.freeboardTitle}
@@ -67,24 +78,22 @@ function FreeBoardDetail() {
               />
               <FreeBoardDelete freeBoardSeq={freeboardSeq} />
             </div>
-          </div>
-          <div className={styles.headInfo2}>
-            <div>{post.userEmail}</div>
-            <div>작성 날짜: {post.freeboardRegisterdate}</div>
+          )}
+        </div>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.title}>{post.freeboardTitle}</div>
+        <div className={styles.contents}>
+          {/* <img src={img} alt="게시글 이미지" /> */}
+          {img && (
+            <img src={img} alt="게시글 이미지" className={styles.contentImg} />
+          )}
+          <div className={styles.contentText}>
+            <div>{post.freeboardContent}</div>
           </div>
         </div>
       </div>
-      <div className={styles.title}>{post.freeboardTitle}</div>
-      <div className={styles.contents}>
-        {/* <img src={img} alt="게시글 이미지" /> */}
-        {img && (
-          <img src={img} alt="게시글 이미지" className={styles.contentImg} />
-        )}
-        <div className={styles.horizon} />
-        <div className={styles.contentText}>
-          <div>{post.freeboardContent}</div>
-        </div>
-      </div>
+      {/* <div className={styles.horizon} /> */}
       {/* <div>해시태그: {post.hashTag}</div> */}
 
       <Comment comments={post.comments} freeboardSeq={post.freeboardSeq} />
