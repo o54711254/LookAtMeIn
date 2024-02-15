4380bb652751d2275ab8dd480c6d23e2131f0eb3
김희수 <wlskaka4@gmail.com>
2024-02-15 13:39:09 +0900

    feat: application.properties

--- B206-frontend-repo/init.sh
+++ B206-frontend-repo/init.sh
@@ -1,5 +1,5 @@
-docker build --no-cache -t frontend-image .
+sudo docker build --no-cache -t frontend-image .
 
-docker rm frontend-container --force
+sudo docker rm frontend-container --force
 
-docker run -p 3000:80 -d --name frontend-container frontend-image
+sudo docker run -p 3000:80 -d --name frontend-container frontend-image
