86ce5979b2384205827b205ec1eae75f750ac982
gyuri <jinry0034@naver.com>
2024-02-15 17:04:55 +0900

    d

--- B206-frontend-repo/src/components/ReviewBoard/ReviewDelete.js
+++ B206-frontend-repo/src/components/ReviewBoard/ReviewDelete.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 import axiosApi from "../../api/axiosApi";
 import { useNavigate, useParams } from "react-router-dom";
+import styles from "./ReviewDelete.module.css";
 
 function ReviewDelete({ reviewBoard_seq, onUpdated }) {
   const navigate = useNavigate();
